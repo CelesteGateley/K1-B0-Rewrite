26965ac835426782b473129cab767845c290821a
Celeste Gateley <celeste.gateley@gmail.com>
2023-07-19 02:48:36 +0100

    Removed option from ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,22 +1,13 @@
-import { Client, SlashCommandBooleanOption, CommandInteraction } from 'discord.js';
-import { Command, OptionsContainer } from '../command';
-import { logger } from '../logger';
+import { Client, CommandInteraction } from 'discord.js';
+import { Command } from '../command';
 
 export const Ping: Command = {
     name: 'ping',
     description: 'Test the bots interaction',
-    options: [
-        new SlashCommandBooleanOption().setName('type').setDescription('yes or no'),
-    ],
     run: async (client: Client, interaction: CommandInteraction) => {
-        const options = new OptionsContainer(interaction);
-        const content = 'Pong!';
-
-        logger.info('' + options.getBoolean('type'));
-
         await interaction.followUp({
             ephemeral: true,
-            content,
+            content: 'Pong!',
         });
     },
 };

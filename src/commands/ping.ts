8e3e74b9f42c7fc2bc89d3c21b0c973599c27f38
Celeste Gateley <celeste.gateley@gmail.com>
2023-07-19 01:29:22 +0100

    Added temporary option to ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,15 +1,21 @@
-import { CommandInteraction, Client } from 'discord.js';
-import { Command } from '../command';
+import { Client, SlashCommandBooleanOption, CommandInteraction } from 'discord.js';
+import { Command, OptionsContainer } from '../command';
 
 export const Ping: Command = {
-	name: 'ping',
-	description: 'Test the bots interaction',
-	run: async (client: Client, interaction: CommandInteraction) => {
-		const content = 'Pong!';
+    name: 'ping',
+    description: 'Test the bots interaction',
+    options: [
+        new SlashCommandBooleanOption().setName('type').setDescription('yes or no'),
+    ],
+    run: async (client: Client, interaction: CommandInteraction) => {
+        const options = new OptionsContainer(interaction);
+        const content = 'Pong!';
 
-		await interaction.followUp({
-			ephemeral: true,
-			content,
-		});
-	},
+        console.log(options.getBoolean('type'));
+
+        await interaction.followUp({
+            ephemeral: true,
+            content,
+        });
+    },
 };

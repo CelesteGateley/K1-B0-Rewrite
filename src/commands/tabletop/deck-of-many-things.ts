0308aa8653eac7f5935ba93d80ea0bba1b514187
Celeste Gateley <celeste.gateley@gmail.com>
2023-07-22 01:54:40 +0100

    added all flag to deck of many things command

--- src/commands/tabletop/deck-of-many-things.ts
+++ src/commands/tabletop/deck-of-many-things.ts
@@ -1,18 +1,30 @@
-import { Client, CommandInteraction } from 'discord.js';
-import { getAssetPath } from '../../command';
+import { Client, CommandInteraction, SlashCommandBooleanOption } from 'discord.js';
+import { getAssetPath, OptionsContainer } from '../../command';
 import fs from 'fs';
 
 export default {
     name: 'deck-of-many-things',
     description: 'Pull from the deck of many things',
+    options: [
+        new SlashCommandBooleanOption().setName('all').setDescription('Show all cards?'),
+    ],
     run: async (client: Client, interaction: CommandInteraction) => {
+        const options = new OptionsContainer(interaction);
         const deck: Map<string, string> = new Map(Object.entries(JSON.parse(fs.readFileSync(getAssetPath('many-things.json'), 'utf8'))));
 
-        const keys = Array.from(deck.keys());
-        const card = keys[Math.floor(Math.random() * keys.length)];
+        if (options.getBoolean('all') ?? false) {
+            let response = '';
+            deck.forEach((card, name) => response += `**${name}**: ${card}\n`);
+            return await interaction.followUp({
+                content: response,
+            });
+        } else {
+            const keys = Array.from(deck.keys());
+            const card = keys[Math.floor(Math.random() * keys.length)];
 
-        await interaction.followUp({
-            content: `You drew **${card}**\n${deck.get(card)}`,
-        });
+            await interaction.followUp({
+                content: `You drew **${card}**\n${deck.get(card)}`,
+            });
+        }
     },
 };
